907d14e7f8252ca32640bf54546113f15e3edea5
Jeroen Mols <jmols.mobile@gmail.com>
2019-03-11 22:01:40 +0100

    Basic multi module project

---
+++ features/login/src/main/java/modularization/login/LoggedInFragment.kt
@@ -0,0 +1,13 @@
+package modularization.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class LoggedInFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_loggedin, container, false);
+    }
+}
---
+++ features/login/src/main/java/modularization/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package modularization.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
