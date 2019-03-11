080a19c1d7d28442a8d7fb8b64fb76bd2071261d
Jeroen Mols <jmols.mobile@gmail.com>
2019-03-11 23:30:27 +0100

    Added navigation graph to dashboard feature

---
+++ features/dashboard/src/main/java/modularization/dashboard/DashboardActivity.kt
@@ -0,0 +1,11 @@
+package modularization.dashboard
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
---
+++ features/dashboard/src/main/java/modularization/dashboard/PhotosFragment.kt
@@ -0,0 +1,13 @@
+package modularization.dashboard
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class PhotosFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_photos, container, false)
+    }
+}
