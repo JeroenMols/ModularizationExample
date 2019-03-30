90f31e7101f4a762ebc8d3644dcbc810da54ee78
Jeroen Mols <jmols.mobile@gmail.com>
2019-03-30 22:51:10 +0100

    Add tests for Actions

---
+++ libraries/actions/src/test/java/modularization/libraries/actions/UserArgsTest.kt
@@ -0,0 +1,13 @@
+package modularization.libraries.actions
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class UserArgsTest {
+
+    @Test
+    fun `test business logic of custom args classes`() {
+        // if there is any business logic in the ***Args classes, it should be tested
+        assertTrue(true)
+    }
+}
