ea571fd7b6069d3afcabc836d6bda95a66a623a9
Jeroen Mols <jmols.mobile@gmail.com>
2019-03-30 22:28:45 +0100

    Add tests for UI components

---
+++ libraries/ui-components/src/androidTest/java/modularization/ui_components/MyCustomComponentUITest.kt
@@ -0,0 +1,13 @@
+package modularization.ui_components
+
+import org.junit.Assert
+import org.junit.Test
+
+class MyCustomComponentUITest {
+
+    @Test
+    fun customComponent_ui_doeswhatitshould() {
+        // e.g. colors, displayed text, enabled/disabled states,...
+        Assert.assertTrue(true)
+    }
+}
---
+++ libraries/ui-components/src/test/java/modularization/ui_components/MyCustomComponentTest.kt
@@ -0,0 +1,13 @@
+package modularization.ui_components
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class MyCustomComponentTest {
+
+    @Test
+    fun `custom component businesslogic does what it should do`() {
+        // e.g. Color conversions, data conversions (value to percent),...
+        assertTrue(true)
+    }
+}
